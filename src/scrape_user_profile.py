e6811ab40793433c4fd56b9ad7621ba65ec1a3b6
topefolorunso <folorunsotemitope10@gmail.com>
2023-03-31 23:15:55 +0100

    try selenium for scraping

--- src/scrape_user_profile.py
+++ src/scrape_user_profile.py
@@ -9,11 +9,11 @@ def scrape_user_profile(username):
     session = HTMLSession()
     url = "https://twitter.com/{}".format(username)
     r = session.get(url)
+    r.html.render(timeout=20)
     return r
 
 def parse_user_profile(response):
-    response.html.render(wait = 10)
-    birthday_xpath = "//span[@data-testid='UserBirthdate']"
+    birthday_xpath = '//span[@data-testid="UserBirthdate"]'
     birthdays = response.html.xpath(birthday_xpath)
     birthday_texts = [
         birthday.text for birthday in birthdays
@@ -35,30 +35,34 @@ def get_user_birthday(username):
         return None
 
 
-followers = [{
-        "id": "2382418134",
-        "name": "omotosho",
-        "username": "toshiey_lz"
-    },
-    {
-        "id": "1518491347245801472",
-        "name": "Tijo",
-        "username": "temiakanmode"
-    },
-    {
-        "id": "199206734",
-        "name": "Vally B",
-        "username": "Vall_erie"
-    }]
+# followers = [{
+#         "id": "2382418134",
+#         "name": "omotosho",
+#         "username": "toshiey_lz"
+#     },
+#     {
+#         "id": "1518491347245801472",
+#         "name": "Tijo",
+#         "username": "temiakanmode"
+#     },
+#     {
+#         "id": "199206734",
+#         "name": "Vally B",
+#         "username": "Vall_erie"
+#     }]
 
-followers_usernames = [
-    follower.get('username') 
-    for follower in followers
-    ]
+# followers_usernames = [
+#     follower.get('username') 
+#     for follower in followers
+#     ]
 
-followers_birthdays = [
-    get_user_birthday(username) 
-    for username in followers_usernames
-    ]
+# followers_birthdays = [
+#     get_user_birthday(username) 
+#     for username in followers_usernames
+#     ]
 
-print(followers_birthdays)
+# print(followers_birthdays)
+
+username = "folorunso__"
+bday = get_user_birthday(username)
+print(bday)
---
+++ src/selenium/twitter_browser.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.support.ui import WebDriverWait
+
+import time
+
+
+
+class Browser():
+    OPTIONS = Options()
+    OPTIONS.add_argument('--no-sandbox')
+    OPTIONS.add_argument('--window-size=1420,1080')
+    # OPTIONS.add_argument('--headless')
+    OPTIONS.add_argument('--disable-gpu')
+
+    DRIVER_PATH = './chromedriver.exe'
+        
+    def __init__(self, url) -> None:
+        self.url = url
+        self.browser = webdriver.Chrome(executable_path=self.DRIVER_PATH, options=self.OPTIONS)
+        self.browser.get(self.url)
+        time.sleep(5)
+        print('webpage opened in background')
+  
+    def close(self):
+        self.browser.close()
+
+
+class TwitterBrowser(Browser):
+
+    def __init__(self, url) -> None:
+        super().__init__(url=url)
+        
+    def get_user_birthday(self):
+        birthday_xpath = '//span[@data-testid="UserBirthdate"]'
+        birthdays = self.browser.find_elements(By.XPATH, birthday_xpath)
+        birthday_texts = [
+            birthday.text for birthday in birthdays
+            ]
+        return birthday_texts[0] if len(birthday_texts) else None

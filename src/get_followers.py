f8cec04b9f8595ba144e505f25ce4ab8e45f9a0b
Tope Folorunso <folorunsotemitope10@gmail.com>
2023-04-08 23:35:32 +0100

    refactor

--- src/get_followers.py
+++ src/get_followers.py
@@ -1,73 +1,46 @@
-# import the module
-import json
-import os
-import requests
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# assign the values accordingly
-consumer_key = os.getenv('APIKey')
-consumer_secret = os.getenv('APIKeySecret')
-access_token = os.getenv('AccessToken')
-access_token_secret = os.getenv('AccessTokenSecret')
-bearer_token = os.getenv('BearerToken')
-
-headers = {"Authorization": "Bearer {}".format(bearer_token)}
+from .database.mongo import get_mongodb_collection
+from .util.helper_functions import (get_or_create_new_user, get_100_followers)
 
 
 
-def bearer_oauth(r):
-    """
-    Method required by bearer token authentication.
-    """
-    r.headers["Authorization"] = f"Bearer {bearer_token}"
-    r.headers["User-Agent"] = "v2FollowersLookupPython"
-    return r
+def main(username: str):
 
-
-def connect_to_endpoint(url, params=None):
-    response = requests.request("GET", url, auth=bearer_oauth, params=params)
-    if response.status_code != 200:
-        raise Exception(
-            "Request returned an error: {} {}".format(
-                response.status_code, response.text
-            )
-        )
-    return response.json()
-
-def get_user_id(screen_name):
-    url = f'https://api.twitter.com/1.1/users/lookup.json?screen_name={screen_name}'
-    json_response = connect_to_endpoint(url)
-    return json_response[0]['id']
-
-def get_100_followers(user_id, next_token):
-    url =  "https://api.twitter.com/2/users/{}/followers".format(user_id)
-    params = {"max_results": 100, "pagination_token": next_token}
-    json_response = connect_to_endpoint(url, params)
-
-    next_100_followers = json_response['data']
-    try:
-        next_token = json_response['meta']['next_token']
-    except KeyError:
-        next_token = False
+    print('retreiving user id ...')
+    user_id = get_or_create_new_user(username= username)
     
-    return next_100_followers, next_token
-
-
-def main():
-    user_id = get_user_id('folorunso__')
+    print('getting followers list ...')
     next_token = None
-    json_data = []
+    followers_list = []
     while next_token or next_token is None:
         next_100_followers, next_token = get_100_followers(user_id, next_token)
-        json_data.extend(next_100_followers)
-    
-    return json_data
-    print(json.dumps(json_data))
+        followers_list.extend(next_100_followers)
+        print('100 followers added ...')
+
+    followers_list = [{
+            "user_id": follower['id'], 
+            "username": follower['username']
+            } 
+            for follower in followers_list
+        ]
+
+    # followers_id = map(get_or_create_new_user, None, followers_list)
+    print('adding followers to database ...')
+    followers_id = [
+        get_or_create_new_user(follower) 
+            for follower in followers_list
+        ]
+
+    users_collection = get_mongodb_collection("users")
+    print('updating user\'s followers list ...')
+    users_collection.update_one(
+        {'user_id': user_id},
+        { "$set": { "followers_id": followers_id } }
+    )
+
+    return True
+
 
 
 if __name__ == "__main__":
-    print(main())
-
-
+    username = 'folorunso__'
+    main(username= username)
